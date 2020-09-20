96c817178ba910529c5bb574e5f2f242be0fafc5
Michael Wang <michael.secure.1@icloud.com>
2020-09-20 00:52:58 -0700

    Added progress bar, removed outdated folder structure, remove deprecated projects, reconfigured to new icon, updates to ApplicationView

--- FileShare/ApplicationView.xaml.cs
+++ FileShare/ApplicationView.xaml.cs
@@ -1,18 +1,9 @@
 ﻿// Using System
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FileShare
 {
@@ -26,6 +17,7 @@ namespace FileShare
             InitializeComponent();
         }
 
+        // Mouse Enter Tooltip
         private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
         {
             tt_send.Visibility = Visibility.Visible;
@@ -33,5 +25,30 @@ namespace FileShare
             tt_settings.Visibility = Visibility.Visible;
 
         }
-    }
+
+		// Progress Bar
+		private void Window_ContentRendered(object sender, EventArgs e)
+		{
+			BackgroundWorker worker = new BackgroundWorker();
+			worker.WorkerReportsProgress = true;
+			worker.DoWork += worker_DoWork;
+			worker.ProgressChanged += worker_ProgressChanged;
+
+			worker.RunWorkerAsync();
+		}
+
+		void worker_DoWork(object sender, DoWorkEventArgs e)
+		{
+			for (int i = 0; i < 100; i++)
+			{
+				(sender as BackgroundWorker).ReportProgress(i);
+				Thread.Sleep(100);
+			}
+		}
+
+		void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+		{
+			FileSendReceiveProgress.Value = e.ProgressPercentage;
+		}
+	}
 }
